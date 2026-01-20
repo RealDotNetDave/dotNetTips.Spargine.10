e7bcf41ab5d7a30356b8bef3c6de3eb3020e42d7
David McCarter <dotnetdave@live.com>
2026-01-20 05:39:36 -0600

    Update file header with new last modified date
    Updated the "Last Modified On" date in the ArrayExtensionsCollectionBenchmark.cs file header comment from 01-03-2026 to 01-19-2026 to reflect recent changes.

--- source/Benchmarking/dotNetTips.Spargine.Extensions.BenchmarkTests/Extensions/ArrayExtensionsCollectionBenchmark.cs
+++ source/Benchmarking/dotNetTips.Spargine.Extensions.BenchmarkTests/Extensions/ArrayExtensionsCollectionBenchmark.cs
@@ -4,7 +4,7 @@
 // Created          : 11-13-2021
 //
 // Last Modified By : David McCarter
-// Last Modified On : 01-03-2026
+// Last Modified On : 01-19-2026
 // ***********************************************************************
 // <copyright file="ArrayExtensionsCollectionBenchmark.cs" company="dotNetTips.com - McCarter Consulting">
 //     David McCarter
