b6c1d302cc9169ee8948cff143007fae40d5d34c
David McCarter <dotnetdave@live.com>
2026-05-14 06:49:15 -0500

    Reformat benchmark code and update group comments
    Re-indented CollectionExtensionsMutatingCollectionBenchmark.cs for consistency and clarity; changed _peopleRefCollection initialization to explicit constructor. Updated Program.cs benchmark group comments to match actual type ranges and improved array formatting. No logic or functional changes.

--- Source/Benchmarking/dotNetTips.Spargine.Extensions.BenchmarkTests/Extensions/CollectionExtensionsMutatingCollectionBenchmark.cs
+++ Source/Benchmarking/dotNetTips.Spargine.Extensions.BenchmarkTests/Extensions/CollectionExtensionsMutatingCollectionBenchmark.cs
@@ -32,228 +32,228 @@ namespace DotNetTips.Spargine.Extensions.BenchmarkTests;
 [TailCallDiagnoser]
 public class CollectionExtensionsMutatingCollectionBenchmark : LargeCollectionBenchmark
 {
-    private Person _existingPerson = default!;
-    private Collection<Person> _peopleRefCollection = default!;
-    private HashSet<Person> _peopleRefHashSet = default!;
-    private List<Person> _peopleRefList = default!;
-    private Person _personToInsert = default!;
+	private Person _existingPerson = default!;
+	private Collection<Person> _peopleRefCollection = default!;
+	private HashSet<Person> _peopleRefHashSet = default!;
+	private List<Person> _peopleRefList = default!;
+	private Person _personToInsert = default!;
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddIf) + ": Condition False")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void AddIfConditionFalse()
-    {
-        this._peopleRefList.AddIf(this._personToInsert, false);
+	[Benchmark(Description = nameof(CollectionExtensions.AddIf) + ": Condition False")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void AddIfConditionFalse()
+	{
+		this._peopleRefList.AddIf(this._personToInsert, false);
 
-        this.Consume(this._peopleRefList);
-    }
+		this.Consume(this._peopleRefList);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddIf) + ": Condition True")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void AddIfConditionTrue()
-    {
-        this._peopleRefList.AddIf(this._personToInsert, true);
+	[Benchmark(Description = nameof(CollectionExtensions.AddIf) + ": Condition True")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void AddIfConditionTrue()
+	{
+		this._peopleRefList.AddIf(this._personToInsert, true);
 
-        this.Consume(this._peopleRefList);
-    }
+		this.Consume(this._peopleRefList);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddIfNotExists) + ": Existing Item")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void AddIfNotExistsExistingItem()
-    {
-        var person = this._existingPerson;
+	[Benchmark(Description = nameof(CollectionExtensions.AddIfNotExists) + ": Existing Item")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void AddIfNotExistsExistingItem()
+	{
+		var person = this._existingPerson;
 
-        var result = this._peopleRefList.AddIfNotExists(person);
+		var result = this._peopleRefList.AddIfNotExists(person);
 
-        this.Consume(result);
-    }
+		this.Consume(result);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddIfNotExists) + ": Existing Item - Collection")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void AddIfNotExistsExistingItemCollection()
-    {
-        var person = this._existingPerson;
+	[Benchmark(Description = nameof(CollectionExtensions.AddIfNotExists) + ": Existing Item - Collection")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void AddIfNotExistsExistingItemCollection()
+	{
+		var person = this._existingPerson;
 
-        var result = this._peopleRefCollection.AddIfNotExists(person);
+		var result = this._peopleRefCollection.AddIfNotExists(person);
 
-        this.Consume(result);
-    }
+		this.Consume(result);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddIfNotExists) + ": Existing Item - HashSet")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void AddIfNotExistsExistingItemHashSet()
-    {
-        var person = this._existingPerson;
+	[Benchmark(Description = nameof(CollectionExtensions.AddIfNotExists) + ": Existing Item - HashSet")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void AddIfNotExistsExistingItemHashSet()
+	{
+		var person = this._existingPerson;
 
-        var result = this._peopleRefHashSet.AddIfNotExists(person);
+		var result = this._peopleRefHashSet.AddIfNotExists(person);
 
-        this.Consume(result);
-    }
+		this.Consume(result);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddIfNotExists) + ": New Item")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void AddIfNotExistsNewItem()
-    {
-        var person = this._personToInsert;
+	[Benchmark(Description = nameof(CollectionExtensions.AddIfNotExists) + ": New Item")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void AddIfNotExistsNewItem()
+	{
+		var person = this._personToInsert;
 
-        var result = this._peopleRefList.AddIfNotExists(person);
+		var result = this._peopleRefList.AddIfNotExists(person);
 
-        this.Consume(result);
-    }
+		this.Consume(result);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddIfNotExists) + ": New Item - Collection")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void AddIfNotExistsNewItemCollection()
-    {
-        var person = this._personToInsert;
+	[Benchmark(Description = nameof(CollectionExtensions.AddIfNotExists) + ": New Item - Collection")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void AddIfNotExistsNewItemCollection()
+	{
+		var person = this._personToInsert;
 
-        var result = this._peopleRefCollection.AddIfNotExists(person);
+		var result = this._peopleRefCollection.AddIfNotExists(person);
 
-        this.Consume(result);
-    }
+		this.Consume(result);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddIfNotExists) + ": New Item - HashSet")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void AddIfNotExistsNewItemHashSet()
-    {
-        var person = this._personToInsert;
+	[Benchmark(Description = nameof(CollectionExtensions.AddIfNotExists) + ": New Item - HashSet")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void AddIfNotExistsNewItemHashSet()
+	{
+		var person = this._personToInsert;
 
-        var result = this._peopleRefHashSet.AddIfNotExists(person);
+		var result = this._peopleRefHashSet.AddIfNotExists(person);
 
-        this.Consume(result);
-    }
+		this.Consume(result);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddRange) + ": Collection")]
-    public void AddRangeCollection()
-    {
-        var result = this._peopleRefCollection.AddRange(this.GetPersonRefCollectionToInsert(), true);
+	[Benchmark(Description = nameof(CollectionExtensions.AddRange) + ": Collection")]
+	public void AddRangeCollection()
+	{
+		var result = this._peopleRefCollection.AddRange(this.GetPersonRefCollectionToInsert(), true);
 
-        this.Consume(result);
-    }
+		this.Consume(result);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddRange) + ": Collection - Non-Unique")]
-    public void AddRangeCollectionNonUnique()
-    {
-        var result = this._peopleRefCollection.AddRange(this.GetPersonRefCollectionToInsert(), false);
+	[Benchmark(Description = nameof(CollectionExtensions.AddRange) + ": Collection - Non-Unique")]
+	public void AddRangeCollectionNonUnique()
+	{
+		var result = this._peopleRefCollection.AddRange(this.GetPersonRefCollectionToInsert(), false);
 
-        this.Consume(result);
-    }
+		this.Consume(result);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddRange) + ": HashSet")]
-    public void AddRangeHashSet()
-    {
-        var result = this._peopleRefHashSet.AddRange(this.GetPersonRefCollectionToInsert(), true);
+	[Benchmark(Description = nameof(CollectionExtensions.AddRange) + ": HashSet")]
+	public void AddRangeHashSet()
+	{
+		var result = this._peopleRefHashSet.AddRange(this.GetPersonRefCollectionToInsert(), true);
 
-        this.Consume(result);
-    }
+		this.Consume(result);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddRange) + ": HashSet - Non-Unique")]
-    public void AddRangeHashSetNonUnique()
-    {
-        var result = this._peopleRefHashSet.AddRange(this.GetPersonRefCollectionToInsert(), false);
+	[Benchmark(Description = nameof(CollectionExtensions.AddRange) + ": HashSet - Non-Unique")]
+	public void AddRangeHashSetNonUnique()
+	{
+		var result = this._peopleRefHashSet.AddRange(this.GetPersonRefCollectionToInsert(), false);
 
-        this.Consume(result);
-    }
+		this.Consume(result);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddRange) + ": List")]
-    public void AddRangeList()
-    {
-        var result = this._peopleRefList.AddRange(this.GetPersonRefCollectionToInsert(), true);
+	[Benchmark(Description = nameof(CollectionExtensions.AddRange) + ": List")]
+	public void AddRangeList()
+	{
+		var result = this._peopleRefList.AddRange(this.GetPersonRefCollectionToInsert(), true);
 
-        this.Consume(result);
-    }
+		this.Consume(result);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.AddRange) + ": List - Non-Unique")]
-    public void AddRangeListNonUnique()
-    {
-        var result = this._peopleRefList.AddRange(this.GetPersonRefCollectionToInsert(), false);
+	[Benchmark(Description = nameof(CollectionExtensions.AddRange) + ": List - Non-Unique")]
+	public void AddRangeListNonUnique()
+	{
+		var result = this._peopleRefList.AddRange(this.GetPersonRefCollectionToInsert(), false);
 
-        this.Consume(result);
-    }
+		this.Consume(result);
+	}
 
-    [IterationSetup]
-    public void Reset()
-    {
-        var people = this.GetPersonRefArray();
-        this._peopleRefList = [.. people];
-        this._peopleRefCollection = people.ToCollection();
-        this._peopleRefHashSet = [.. people];
-    }
+	[IterationSetup]
+	public void Reset()
+	{
+		var people = this.GetPersonRefArray();
+		this._peopleRefList = [.. people];
+		this._peopleRefCollection = new Collection<Person>([.. people]);
+		this._peopleRefHashSet = [.. people];
+	}
 
-    /// <summary>
-    /// Setups this instance.
-    /// </summary>
-    public override void Setup()
-    {
-        base.Setup();
+	/// <summary>
+	/// Setups this instance.
+	/// </summary>
+	public override void Setup()
+	{
+		base.Setup();
 
-        var people = this.GetPersonRefArray();
-        this._existingPerson = people[0];
-        this._personToInsert = this.GetPersonRefCollectionToInsert()[0];
-    }
+		var people = this.GetPersonRefArray();
+		this._existingPerson = people[0];
+		this._personToInsert = this.GetPersonRefCollectionToInsert()[0];
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.Upsert) + ": Existing Item")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void UpsertExistingItem()
-    {
-        var person = this._existingPerson;
+	[Benchmark(Description = nameof(CollectionExtensions.Upsert) + ": Existing Item")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void UpsertExistingItem()
+	{
+		var person = this._existingPerson;
 
-        this._peopleRefList.Upsert(person);
+		this._peopleRefList.Upsert(person);
 
-        this.Consume(this._peopleRefList);
-    }
+		this.Consume(this._peopleRefList);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.Upsert) + ": Existing Item - Collection")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void UpsertExistingItemCollection()
-    {
-        var person = this._existingPerson;
+	[Benchmark(Description = nameof(CollectionExtensions.Upsert) + ": Existing Item - Collection")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void UpsertExistingItemCollection()
+	{
+		var person = this._existingPerson;
 
-        this._peopleRefCollection.Upsert(person);
+		this._peopleRefCollection.Upsert(person);
 
-        this.Consume(this._peopleRefCollection);
-    }
+		this.Consume(this._peopleRefCollection);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.Upsert) + ": Existing Item - HashSet")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void UpsertExistingItemHashSet()
-    {
-        var person = this._existingPerson;
+	[Benchmark(Description = nameof(CollectionExtensions.Upsert) + ": Existing Item - HashSet")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void UpsertExistingItemHashSet()
+	{
+		var person = this._existingPerson;
 
-        this._peopleRefHashSet.Upsert(person);
+		this._peopleRefHashSet.Upsert(person);
 
-        this.Consume(this._peopleRefHashSet);
-    }
+		this.Consume(this._peopleRefHashSet);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.Upsert) + ": New Item")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void UpsertNewItem()
-    {
-        var person = this._personToInsert;
+	[Benchmark(Description = nameof(CollectionExtensions.Upsert) + ": New Item")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void UpsertNewItem()
+	{
+		var person = this._personToInsert;
 
-        this._peopleRefList.Upsert(person);
+		this._peopleRefList.Upsert(person);
 
-        this.Consume(this._peopleRefList);
-    }
+		this.Consume(this._peopleRefList);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.Upsert) + ": New Item - Collection")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void UpsertNewItemCollection()
-    {
-        var person = this._personToInsert;
+	[Benchmark(Description = nameof(CollectionExtensions.Upsert) + ": New Item - Collection")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void UpsertNewItemCollection()
+	{
+		var person = this._personToInsert;
 
-        this._peopleRefCollection.Upsert(person);
+		this._peopleRefCollection.Upsert(person);
 
-        this.Consume(this._peopleRefCollection);
-    }
+		this.Consume(this._peopleRefCollection);
+	}
 
-    [Benchmark(Description = nameof(CollectionExtensions.Upsert) + ": New Item - HashSet")]
-    [BenchmarkCategory(Categories.Collections)]
-    public void UpsertNewItemHashSet()
-    {
-        var person = this._personToInsert;
+	[Benchmark(Description = nameof(CollectionExtensions.Upsert) + ": New Item - HashSet")]
+	[BenchmarkCategory(Categories.Collections)]
+	public void UpsertNewItemHashSet()
+	{
+		var person = this._personToInsert;
 
-        this._peopleRefHashSet.Upsert(person);
+		this._peopleRefHashSet.Upsert(person);
 
-        this.Consume(this._peopleRefHashSet);
-    }
+		this.Consume(this._peopleRefHashSet);
+	}
 }
