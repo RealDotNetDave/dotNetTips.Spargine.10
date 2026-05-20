d6546e9a37df47a02262603ecd026e2264d45d51
David McCarter <dotnetdave@live.com>
2026-05-20 06:54:20 -0500

    Enable running all benchmarks by default
    Uncommented `BenchmarkHelper.RunAllBenchmarks(config)` to execute all available benchmarks. Commented out the selective benchmark run to ensure the full suite is tested.

--- source/Benchmarking/dotNetTips.Spargine.Core.BenchmarkTests/Program.cs
+++ source/Benchmarking/dotNetTips.Spargine.Core.BenchmarkTests/Program.cs
@@ -20,8 +20,6 @@ using BenchmarkDotNet.Environments;
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Reports;
 using DotNetTips.Spargine.Benchmarking;
-using DotNetTips.Spargine.Core.BenchmarkTests.Collections.Generic.Concurrent;
-using DotNetTips.Spargine.Core.BenchmarkTests.Security;
 using Perfolizer.Horology;
 
 //'![](7050BB9CE02F97B17501B57A581147A7.png;https://bit.ly/Spargine ;;0.01188,0.01188)
@@ -46,10 +44,10 @@ internal sealed class Program
 		config = config.WithOption(ConfigOptions.DisableOptimizationsValidator, true);
 
 		// Run All Tests
-		//BenchmarkHelper.RunAllBenchmarks(config);
+		BenchmarkHelper.RunAllBenchmarks(config);
 
 		// Run Selected Tests
-		BenchmarkHelper.RunBenchmarks(config, true, typeof(DistinctBlockingCollectionMutatingCollectionBenchmark), typeof(TypeHelperBenchmark), typeof(UlidBenchmark));
+		//BenchmarkHelper.RunBenchmarks(config, true, typeof(DistinctBlockingCollectionMutatingCollectionBenchmark), typeof(TypeHelperBenchmark), typeof(UlidBenchmark));
 
 	}
 }
